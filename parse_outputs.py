2224dae81f43cc496bd38389264c63555ea1cc6d
Can Abdulla <canabdulla@outlook.de>
2024-07-19 13:39:13 +0200

    Add support for baseline_sampling_test

--- parse_outputs.py
+++ parse_outputs.py
@@ -7,6 +7,7 @@ import datetime
 import platform
 import cpuinfo
 
+
 def save_csv(data, dest):
     df = pd.DataFrame(data,
                       columns=["algorithm", "dataset", "M", "subcentroids", "separate", 'metric', 'value'])
@@ -17,6 +18,7 @@ def save_csv(data, dest):
         df.to_csv(dest, header=True, index=False)
     else:
         df.to_csv(dest, mode='a', header=False, index=False)
+
 
 def parse_perf_output(output_dir):
     data = []
@@ -37,11 +39,10 @@ def parse_perf_output(output_dir):
     return data
 
 
-def parse_dist_output(output_dir):
+def parse_output(output_dir, metrics):
     data = []
     for filename in os.listdir(output_dir):
         val = np.loadtxt(open(os.path.join(output_dir, filename), "rb"), delimiter=",")
-        metrics = ["distortion", "ms"]
         for i in range(len(val)):
             testcase = os.path.splitext(filename)[0].split()
             testcase.append(metrics[i])
@@ -49,26 +50,21 @@ def parse_dist_output(output_dir):
             data.append(testcase)
     return data
 
-def parse_ann_output(output_dir):
-    data = []
-    for filename in os.listdir(output_dir):
-        val = np.loadtxt(open(os.path.join(output_dir, filename), "rb"), delimiter=",")
-        metrics = ["recall", "ms"]
-        for i in range(len(val)):
-            testcase = os.path.splitext(filename)[0].split()
-            testcase.append(metrics[i])
-            testcase.append(val[i])
-            data.append(testcase)
-    return data
 
 def parse_ml_output(output_dir):
     data = []
     for filename in os.listdir(output_dir):
         dataset = filename.split(' ')[1]
         if dataset == 'KDD98':
-            metrics = ["train mse", "train rsq", "test mse", "test rsq", "ms"]
+            metrics = ["train-mse", "train-rsq", "test-mse", "test-rsq", "ms"]
         else:
-            metrics = ["accuracy", "avg-precision", "avg-recall", "macro-f1", "ms"]
+            if dataset == 'Adult':
+                metrics = ["accuracy", "precision", "recall", "f1"]
+            else:
+                metrics = ["accuracy", "avg-precision", "avg-recall", "macro-f1"]
+            m = [f"train-{i}" for i in metrics]
+            m2 = [f"test-{i}" for i in metrics]
+            metrics = m + m2 + ["ms"]
         val = np.loadtxt(open(os.path.join(output_dir, filename), "rb"), delimiter=",")
         for i in range(len(val)):
             testcase = os.path.splitext(filename)[0].split()
@@ -83,7 +79,7 @@ def main():
         print("Usage: parse_outputs.py <test_case>")
         return
     if sys.argv[1] == "dist":
-        data = parse_dist_output("output/distortion")
+        data = parse_output("output/distortion", ["distortion", "ms"])
         perf_data = parse_perf_output("perf_output/distortion")
         data.extend(perf_data)
         save_csv(data, "results/distortion.csv")
@@ -91,12 +87,17 @@ def main():
         data = parse_ml_output("output/ml")
         perf_data = parse_perf_output("perf_output/ml")
         data.extend(perf_data)
-        save_csv(data, "./results/ml111.csv")
+        save_csv(data, "./results/ml.csv")
     elif sys.argv[1] == "ann":
-        data = parse_ann_output("output/ann")
+        data = parse_output("output/ann", ["recall", "ms"])
         perf_data = parse_perf_output("perf_output/ann")
         data.extend(perf_data)
         save_csv(data, "./results/ann.csv")
+    elif sys.argv[1] == "bs":
+        data = parse_output("output/bs", ["train-accuracy", "test-accuracy", "ms"])
+        perf_data = parse_perf_output("perf_output/bs")
+        data.extend(perf_data)
+        save_csv(data, "./results/bs.csv")
 
 
 if __name__ == '__main__':
