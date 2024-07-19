c2473c755aec7dc8a90884efb637c6c189968d36
Can Abdulla <canabdulla@outlook.de>
2024-07-19 13:42:50 +0200

    Add support for baseline_sampling_test

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -9,17 +9,23 @@ mkdir -p ./data/ml
 mkdir -p ./output/distortion
 mkdir -p ./output/ml
 mkdir -p ./output/ann
+mkdir -p ./output/bs
 
 mkdir -p ./results
 
 mkdir -p ./perf_output/ml
 mkdir -p ./perf_output/distortion
 mkdir -p ./perf_output/ann
+mkdir -p ./perf_output/bs
 
 mkdir -p ./plots/distortion/line/perf
 mkdir -p ./plots/distortion/bar/perf
 mkdir -p ./plots/ml/bar/perf
 mkdir -p ./plots/ml/line/perf
+mkdir -p ./plots/ann/line/perf
+mkdir -p ./plots/ann/bar/perf
+mkdir -p ./plots/bs/line/perf
+mkdir -p ./plots/bs/bar/perf
 
 sudo apt-get install linux-tools-common linux-tools-generic linux-tools-`uname -r`
 sudo apt install unzip
