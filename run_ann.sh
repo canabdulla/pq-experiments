a25fb53cce8c62885de5fe7d89c879dd379e5bd2
Can Abdulla <canabdulla@outlook.de>
2024-07-23 14:20:03 +0200

    Add space decomposition test

--- run_ann.sh
+++ run_ann.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
-CMD="java -Xmx24g -Xms24g -cp ./lib/*:./SystemDS.jar org.apache.sysds.api.DMLScript "
+CMD="java -Xmx12g -Xms12g -cp ./lib/*:./SystemDS.jar org.apache.sysds.api.DMLScript "
 #enable codegeneration
 CONF=" -config dataprep/SystemDS-config.xml" #just for faster training (~7x)
 
 #run the ann tests with perf. results are saved in output/ann and perf_output/ann
 run_ann() {
-  file="ann/PQ $dataset $M $subcentroids TRUE"
+  local alg=$1
+  if [ "$alg" = "PQ" ]; then
+      space_decomp="FALSE"
+  else
+      space_decomp="TRUE"
+  fi
+  file="ann/$alg $dataset $M $subcentroids TRUE"
+#  file="ann/PQ $dataset $M $subcentroids TRUE"
   #start timer
   start=$(date +%s%N)
   #execute dml script with perf
   sudo perf stat -x \; -o "./perf_output/$file" -d -d -d \
    $CMD $CONF -f experiments/ann_test.dml -exec singlenode -stats \
-      -nvargs M=$M subcentroids=$subcentroids  out_file="$file"
+      -nvargs M=$M subcentroids=$subcentroids  out_file="$file" space_decomp=$space_decomp
   #calculate execution time
   end=$(date +%s%N)
   time=$((($end-$start) / 1000000 - 1500))
   #append execution time to output
   if [ -f "./output/$file" ]; then
-      echo "PQ $dataset M=$M subcentroids=$subcentroids" >> successful_ann_runs.txt
       sed -i s/$/,"$time"/ "./output/$file"
-  else
-      echo "PQ $dataset M=$M subcentroids=$subcentroids" >> failed_ann_runs.txt
   fi
 }
 
@@ -30,10 +34,10 @@ execute_runs() {
   local c_list=$2
   dataset=$3
   #run product quantization
-  for centroids in $c_list; do
+  for subcentroids in $c_list; do
     for M in $M_list; do
-      subcentroids=$((centroids / M))
-        run_ann
+        run_ann PQ
+        run_ann PQ-SPACEDECOMP
     done
   done
 }
@@ -43,7 +47,7 @@ rm -f output/ann/*
 rm -f perf_output/ann/*
 
 application="ann"
-execute_runs "1 2 4 8" "8 16 32 64 128 256" "siftsmall_base"
+execute_runs "1 2 4 8 16" "8 16 32 64 128 256 512 1024 2048" "siftsmall_base"
 
 #remove metadata file to ensure correct parsing of outputs
 rm -f output/ann/*.mtd
